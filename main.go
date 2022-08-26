bc8ad6b51e3d161f5dcd9483025cf82ac80aa81e
vzan2012 <vzan2012@gmail.com>
2022-08-26 07:30:15 +0200

    feat: variables declaration

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Variable Declaration can be done here
+var deckSize int
+
+func main() {
+	deckSize = 20
+
+	// Ways of declaring variable
+	var card string = "Ace of Spades"
+	cardNew := "Ace of Spades"
+
+	// Print the variables
+	fmt.Println(card)
+	fmt.Println((cardNew))
+	fmt.Println(deckSize)
+}
