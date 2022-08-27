e8d9dbb09c69ee8d567fa6878da45b72b9a4ad54
vzan2012 <vzan2012@gmail.com>
2022-08-27 17:12:46 +0200

    feat: updating deal functions

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// Slice Example
 	cards := newDeck()
 
 	// Print the deck of cards
-	cards.print()
+	// cards.print()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("----")
+	remainingCards.print()
 }
