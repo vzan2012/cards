05161352e7aeb13abae863ae03a5b864cde991af
Deepak Guptha <vzan2012@gmail.com>
2022-09-03 22:42:34 +0530

    feat: updated test cases and coverage

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -23,4 +24,29 @@ func TestNewDeck(t *testing.T) {
 	if(d[len(d)-1] != expectedLastCard) {
 		t.Errorf("Expected last card to be %v, but got %v", expectedLastCard, d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	expectedCards := 16
+	testFileName := "_decktesting"
+
+	// Remove any test file exists 
+	os.Remove(testFileName)
+
+	// Call the newDeck func 
+	d := newDeck()
+
+	// Save the deck to testfile
+	d.saveToFile(testFileName)
+
+	// Load the Deck testfile
+	loadedDeck := newDeckFromFile(testFileName)
+
+	// Test case
+	if(len(loadedDeck) != expectedCards) {
+		t.Errorf("Expected %v cards in deck, but got %v", expectedCards, len(loadedDeck))
+	}
+
+	// Remove the file 
+	os.Remove(testFileName)
 }
